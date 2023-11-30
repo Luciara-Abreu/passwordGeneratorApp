8edbcca79c1cd094deb39bfaa2b3f1fa0dfd72ba
Luciara-Abreu <luciara.abreu@gmail.com>
2023-11-29 22:59:18 -0300

    add Home

--- App.js
+++ App.js
@@ -1,100 +1,12 @@
-import { useState} from 'react'
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Modal} from 'react-native';
-import Slider from '@react-native-community/slider';
-import {ModalPassword} from './src/components/modal'
-
-
-let charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#%&'
-
-
+import { NavigationContainer } from '@react-navigation/native';
+import { Routes } from './src/routes'
 
 
 export default function App() {
-const [size, setSize] = useState(6)
-const [passwordValue, setPasswordValue] = useState("")
-const [modalVisible, setModalVisible] = useState(false)
-
-
-function generatePassword(){
-  let password = "";
-
-  for(i = 0, n = charset.length; i < size; i++ ){
-    password += charset.charAt(Math.floor(Math.random() * n ))
-  }
-  setPasswordValue(password)
-  setModalVisible(true)
-}
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={require('./src/assets/logo.png')}
-        style={styles.logo} />
-      <StatusBar style="auto" />
-
-      <Text style={styles.title}>{size} Caracteres </Text>
-
-      <View style={styles.area}>
-        <Slider
-          style={{ width: 300, height: 40 }}
-          minimumValue={6}
-          maximumValue={20}
-          minimumTrackTintColor="#392de9"
-          maximumTrackTintColor="gray"
-          thumbTintColor='#392de9'
-          value={size}
-          onValueChange={(value) => setSize(value.toFixed(0))}
-          
-        />
-      </View>
-
-      <TouchableOpacity style={styles.button} onPress={generatePassword}>
-        <Text style={styles.buttonText}>Gerar Senha</Text>
-      </TouchableOpacity>
-
-      <Modal visible={modalVisible} animationType='fade' transparent={true}>
-        <ModalPassword password={passwordValue} handleClose={() => setModalVisible(false)}/>
-      </Modal>
-
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F3F3FF',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  logo: {
-    marginBottom: 60
-  },
-  title:{
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
-  area: {
-    alignItems: 'center',
-    marginTop: 14,
-    marginBottom: 14,
-    width: '80%',
-    backgroundColor: '#FFF',
-    padding: 8,
-    borderRadius: 8,
-  },
-  button:{
-    backgroundColor: '#392de9',
-    width: '80%',
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 8,
-    marginBottom: 28,
-  },
-  buttonText: {
-    color: '#FFF',
-    fontSize: 20,
-  }
-});
---
+++ src/pages/passwords/index.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity, Modal} from 'react-native';
+
+
+export function Passwords(){
+  return (
+    <View>
+      <Text> Minhas senhas:  </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    
+  }
+})
